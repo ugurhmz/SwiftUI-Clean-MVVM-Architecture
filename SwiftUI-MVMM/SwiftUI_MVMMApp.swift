0796f6d369d5168195bc64121733b12a944b39cf
Uğur hamzaoğlu <ugur.hmz52@gmail.com>
2025-12-18 15:58:26 +0300

    Initial Commit

---
+++ SwiftUI-MVMM/SwiftUI_MVMMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVMMApp.swift
+//  SwiftUI-MVMM
+//
+//  Created by rico on 18.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVMMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
